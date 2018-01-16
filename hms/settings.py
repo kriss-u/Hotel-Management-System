82cc7dc2459059d34923612a5f4a6fbc5a4fbce5
Krishna Upadhyay <072bct517krishna@ioe.edu.np>
2018-01-17 00:44:19 +0545

    a

--- hms/settings.py
+++ hms/settings.py
@@ -126,4 +126,4 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
